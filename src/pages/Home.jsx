2e46febd0d26124d0fb3305d16a20017953a268f
rafsan1p <rubayetrafsan605@gmail.com>
2025-11-23 12:12:59 +0600

    Popular Section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from '../component/Slider';
+import PopularSection from '../component/PopularSection';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <PopularSection></PopularSection>
         </div>
     );
 };
